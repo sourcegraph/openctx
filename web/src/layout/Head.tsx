c30ab7349593c49f7df9757120f2f252f4f17b35
Quinn Slack <quinn@slack.org>
2023-12-25 20:55:14 -0600

    set canonical URL for website pages (#4)
    Also upgrade web deps.

--- web/src/layout/Head.tsx
+++ web/src/layout/Head.tsx
@@ -4,14 +4,21 @@ import interItalicFontUrl from '/Inter-italic.var.woff2'
 import interRomanFontUrl from '/Inter-roman.var.woff2'
 import svgIconUrl from '/logomark-v0.svg'
 import { type FunctionComponent } from 'react'
+import { usePageContext } from 'vike-react/usePageContext'
 
-export const Head: FunctionComponent = () => (
-    <>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/svg+xml" href={svgIconUrl} />
-        <link rel="icon" type="image/x-icon" href={icoIconUrl} />
-        <link rel="preload" type="font/woff2" as="font" href={interRomanFontUrl} crossOrigin="" />
-        <link rel="preload" type="font/woff2" as="font" href={interItalicFontUrl} crossOrigin="" />
-        <link rel="preload" type="font/woff2" as="font" href={firaCodeFontUrl} crossOrigin="" />
-    </>
-)
+const CANONICAL_BASE_URL = new URL('https://opencodegraph.org')
+
+export const Head: FunctionComponent = () => {
+    const { urlPathname } = usePageContext()
+    return (
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" type="image/svg+xml" href={svgIconUrl} />
+            <link rel="icon" type="image/x-icon" href={icoIconUrl} />
+            <link rel="preload" type="font/woff2" as="font" href={interRomanFontUrl} crossOrigin="" />
+            <link rel="preload" type="font/woff2" as="font" href={interItalicFontUrl} crossOrigin="" />
+            <link rel="preload" type="font/woff2" as="font" href={firaCodeFontUrl} crossOrigin="" />
+            <link rel="canonical" href={new URL(urlPathname, CANONICAL_BASE_URL).toString()} />
+        </>
+    )
+}
