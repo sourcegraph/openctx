c595293b8f5b80371662c1bcbe4d062e5589309b
Quinn Slack <quinn@slack.org>
2023-12-27 22:28:35 -1000

    bump packages for publishing

--- web/pages/playground/data.ts
+++ web/pages/playground/data.ts
@@ -48,7 +48,7 @@ function providerBundleUrl(packageName: string, importUrl: string): string {
 
 const settings = {
     'opencodegraph.providers': {
-        [providerBundleUrl('@opencodegraph/provider-links@0.0.4', providerLinksUrl)]: {
+        [providerBundleUrl('@opencodegraph/provider-links@0.0.5', providerLinksUrl)]: {
             links: [
                 {
                     title: 'Telemetry',
@@ -73,7 +73,7 @@ const settings = {
                 },
             ],
         } satisfies import('@opencodegraph/provider-links').Settings,
-        [providerBundleUrl('@opencodegraph/provider-prometheus@0.0.2', providerPrometheusUrl)]: {
+        [providerBundleUrl('@opencodegraph/provider-prometheus@0.0.3', providerPrometheusUrl)]: {
             metricRegistrationPatterns: [
                 {
                     path: '**/*.ts?(x)',
@@ -82,7 +82,7 @@ const settings = {
                 },
             ],
         } satisfies import('@opencodegraph/provider-prometheus').Settings,
-        [providerBundleUrl('@opencodegraph/provider-storybook@0.0.2', providerStorybookUrl)]: {
+        [providerBundleUrl('@opencodegraph/provider-storybook@0.0.3', providerStorybookUrl)]: {
             storybookUrl: 'https://daeeaa811098f52f15a110dbaf76b6c416191c3b--5f0f381c0e50750022dc6bf7.chromatic.com/', // this is a public URL because our storybooks are public
         } satisfies import('@opencodegraph/provider-storybook').Settings,
     },
