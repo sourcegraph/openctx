f4bd2389c15adf990e5ae5908693550511204940
Harish Navnit <harishnavnit@gmail.com>
2024-01-08 03:42:58 +0530

    include techstack provider in web playground

--- client/web-playground/src/demo/settings.ts
+++ client/web-playground/src/demo/settings.ts
@@ -1,8 +1,10 @@
+import path from 'path'
 import type { ProviderSettings } from '@openctx/client'
 
 async function getProviders(): Promise<Record<string, ProviderSettings | boolean>> {
     const providerSettings: Record<string, ProviderSettings | boolean> = {
         '../../../../provider/hello-world/index.ts': true,
+        '../../../../provider/techstack/index.ts': { yaml: path.resolve('examples/stackshare.yml') },
         '../../../../provider/links/index.ts': {
             links: [
                 {
