22ed874810ba71e49e326173efd5ddd95cdbdb9c
Harish Navnit <harishnavnit@gmail.com>
2024-01-09 12:01:22 +0530

    Remove node polyfills from techstack provider

--- provider/techstack/vite.config.ts
+++ provider/techstack/vite.config.ts
@@ -1,11 +1,4 @@
 
 import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-
-export default defineConfig({
-    assetsInclude: [ '**/*.yml' ],
-    plugins: [
-        nodePolyfills({ include: ['path'] })
-    ]
-})
+export default defineConfig({ assetsInclude: [ '**/*.yml' ] })
