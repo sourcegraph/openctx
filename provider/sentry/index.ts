b91eedd8938b441badf2d70cd6c0e97b8d87e142
Harish Navnit <harishnavnit@gmail.com>
2024-02-05 13:00:17 +0800

    Show raw annotations

--- provider/sentry/index.ts
+++ provider/sentry/index.ts
@@ -1,4 +1,9 @@
 
+// TODO: Remove this and fix subsequent warnings
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 import {
     type AnnotationsParams,
     type AnnotationsResult,
@@ -7,6 +12,7 @@ import {
     type Provider
 } from '@opencodegraph/provider'
 
+import { Sentry } from './web/client'
 import * as extensions from './extensions'
 
 
@@ -26,13 +32,49 @@ export interface Settings {
 
 const sentry: Provider<Settings> = {
     capabilities(params: CapabilitiesParams, settings: Settings): CapabilitiesResult {
+        return {} // FIXME: Map platform correctly
         return { selector: extensions.filetype(settings.platform || 'null')
                                      .map(ext => new Object({path: ext})) }
     },
 
-    async annotations(params: AnnotationsParams, settings: Settings): AnnotationsResult {
+    async annotations(params: AnnotationsParams, settings: Settings): Promise<AnnotationsResult> {
+        // console.log(`'params': ${JSON.stringify(params, null, 2)}`)
+
+        const client: Sentry = new Sentry(settings)
         const result: AnnotationsResult = { items: [], annotations: [] }
-    }
+
+        // Fetch errors from Sentry
+        const errs: any = await client.errors(settings.organization, settings.project)
+
+        // Gather issues for the current file
+        const issues = Object.create(null)
+        errs.forEach((err: any) => {
+            err.entries.forEach((entry: any) => {
+                // console.log(`${entry.data.formatted}: ${entry.data.frames}`)
+                const frames: string = entry.data.frames ?? []
+                const title: string = entry.data.formatted ?? 'Unknown Error'
+                issues[title] = (issues[title] ?? []).concat(frames)
+            })
+        })
+
+        for (const [title, frames] of Object.entries(issues)) {
+            Object.values(frames).forEach(frame => {
+                if (frame.lineNo <= params.content.split(/\r?\n/).length) {
+                    result.items.push({ id: frame.lineNo, title: `🔺 ${title}` })
+                    result.annotations.push({
+                        item: { id: frame.lineNo },
+                        range: {
+                            start: { line: frame.lineNo, character: 0 },
+                            end: { line: frame.lineNo, character: 1 }
+                        }
+                    })
+                }
+            })
+        }
+
+        console.log(`result: ${JSON.stringify(result, null, 2)}`)
+        return result
+    },
 }
 
 export default sentry
--- provider/sentry/web/client.ts
+++ provider/sentry/web/client.ts
@@ -1,4 +1,9 @@
 
+// TODO: Remove this and fix subsequent warnings
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 import { HTTP } from './http'
 import { type Settings } from '../index'
 
@@ -50,6 +55,21 @@ export class Sentry {
     public async issues(org: string, proj: string): Promise<any> {
         try {
             const r = await this.http.get(`/projects/${org}/${proj}/issues/`)
+            return r.json() ?? r.text
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    /**
+     * Returns all error events for a project with full body event payloads.
+     *
+     * @param org - organization slug
+     * @param proj - project slug
+     */
+    public async errors(org: string, proj: string): Promise<any> {
+        try {
+            const r = await this.http.get(`/projects/${org}/${proj}/events/?full=true`)
             return r.json() ?? r.text
         } catch (e) {
             console.error(e)
