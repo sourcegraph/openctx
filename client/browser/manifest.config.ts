9fed8dcddc5a8bbf679c97739b7b48049db94997
Quinn Slack <quinn@slack.org>
2024-02-24 20:20:58 +0100

    pin Chrome Web Store extension public key to preserve listing across name change

--- client/browser/manifest.config.ts
+++ client/browser/manifest.config.ts
@@ -4,6 +4,7 @@ import packageJson from './package.json'
 const { version } = packageJson
 export default defineManifest(env => ({
     manifest_version: 3,
+    key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlj3eX64ybepLkrNAVzF/kWd8iv9EVstTcKQ+r6BrL68y+raGAwy3eVAZs5Dj6w5YaiPM3yiRrEPu5RV/C0U3aRwehy69CG05bomF02q72ZXbetYX9V1dXvwAzVwC46ZuYSlgr52wifXrDwIfiI0ARS0TcqFZxjwW9W1gQhAG8ETcKrPTZ520JGLjwE0x45/lIkkvJ8RgDc3rrGlhYxEyBquTYAG7Il5BjOmcpDNw/VUyOgnIPVGkqc8CdsXu9GWEn1LlALp2BWN2iqcjFxpmC92qvK+Itl8C9oEP2OlQBWRoEPTSKmMqlj9Gaud7wadAEs0mU9VG3dCdhIMU0/xn9QIDAQAB',
     name: env.mode === 'development' ? 'OpenCtx [dev]' : 'OpenCtx',
     description: "Enhance your code host's UI with contextual info from your other dev tools.",
     version,
