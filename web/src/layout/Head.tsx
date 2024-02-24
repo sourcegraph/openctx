c9afa29bbe8f00c74c9fd72f15a7aee532f54d33
Quinn Slack <quinn@slack.org>
2024-02-24 11:09:47 +0100

    upgrade web dependencies (#24)

--- web/src/layout/Head.tsx
+++ web/src/layout/Head.tsx
@@ -1,17 +1,21 @@
-import icoIconUrl from '/favicon.ico'
+import type { ReactNode } from 'react'
+import { usePageContext } from 'vike-react/usePageContext'
 import firaCodeFontUrl from '/FiraCode.woff2'
 import interItalicFontUrl from '/Inter-italic.var.woff2'
 import interRomanFontUrl from '/Inter-roman.var.woff2'
+import icoIconUrl from '/favicon.ico'
 import svgIconUrl from '/logomark-v0.svg'
-import type { FunctionComponent } from 'react'
-import { usePageContext } from 'vike-react/usePageContext'
 
 const CANONICAL_BASE_URL = new URL('https://openctx.org')
 
-export const Head: FunctionComponent = () => {
+export const Head = (): ReactNode => {
     const { urlPathname } = usePageContext()
     return (
         <>
+            <meta
+                name="description"
+                content="Annotate code with info from other dev tools, in your editor and anywhere else you view code."
+            />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" type="image/svg+xml" href={svgIconUrl} />
             <link rel="icon" type="image/x-icon" href={icoIconUrl} />
