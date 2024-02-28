31bd2c47864684f8fd43eff6eae27c4a810197a4
Quinn Slack <quinn@slack.org>
2024-02-28 13:56:54 +0100

    wip

--- lib/client/src/api.ts
+++ lib/client/src/api.ts
@@ -1,10 +1,4 @@
-import type {
-    AnnotationsParams,
-    AnnotationsResult,
-    ItemsParams,
-    ItemsResult,
-    ProviderSettings,
-} from '@openctx/protocol'
+import type { AnnotationsParams, ItemsParams, ProviderSettings } from '@openctx/protocol'
 import type { Annotation as AnnotationWithPlainRange, Item, Range } from '@openctx/schema'
 import {
     type Observable,
@@ -53,41 +47,50 @@ export interface ObserveOptions {
     emitPartial: boolean
 }
 
-/**
- * Observes OpenCtx items from the configured providers.
- *
- * TODO(sqs): dedupe with observeAnnotations
- */
-export function observeItems(
+function observeProviderCall<R>(
     providerClients: Observable<ProviderClientWithSettings[]>,
-    params: ItemsParams,
+    fn: (provider: ProviderClientWithSettings) => Observable<R[] | null>,
     { emitPartial, logger }: Pick<ClientEnv<never>, 'logger'> & ObserveOptions
-): Observable<Item[]> {
+): Observable<R[]> {
     return providerClients.pipe(
         mergeMap(providerClients =>
             providerClients && providerClients.length > 0
                 ? combineLatest(
-                      providerClients.map(({ providerClient: { items }, settings }) =>
-                          items
-                              ? defer(() => from(items(params, settings))).pipe(
-                                      emitPartial ? startWith(null) : tap(),
-                                      catchError(error => {
-                                          logger?.(`failed to get items: ${error}`)
-                                          console.error(error)
-                                          return of(null)
-                                      })
-                                  )
-                              : of(null)
+                      providerClients.map(({ providerClient, settings }) =>
+                          defer(() => fn({ providerClient, settings })).pipe(
+                              emitPartial ? startWith(null) : tap(),
+                              catchError(error => {
+                                  logger?.(`failed to call provider: ${error}`)
+                                  console.error(error)
+                                  return of(null)
+                              })
+                          )
                       )
                   )
                 : of([])
         ),
-        map(result => result.filter((v): v is ItemsResult => v !== null).flat()),
+        map(result => result.filter((v): v is R[] => v !== null).flat()),
         tap(items => {
             if (LOG_VERBOSE) {
-                logger?.(`got ${items.length} items: ${JSON.stringify(items)}`)
+                logger?.(`got ${items.length} results: ${JSON.stringify(items)}`)
             }
         })
+    )
+}
+
+/**
+ * Observes OpenCtx items from the configured providers.
+ */
+export function observeItems(
+    providerClients: Observable<ProviderClientWithSettings[]>,
+    params: ItemsParams,
+    { logger, emitPartial }: Pick<ClientEnv<never>, 'logger'> & ObserveOptions
+): Observable<Item[]> {
+    return observeProviderCall(
+        providerClients,
+        ({ providerClient, settings }) =>
+            providerClient.items ? from(providerClient.items(params, settings)) : of(null),
+        { logger, emitPartial }
     )
 }
 
@@ -97,44 +100,36 @@ export function observeItems(
 export function observeAnnotations<R extends Range>(
     providerClients: Observable<ProviderClientWithSettings[]>,
     params: AnnotationsParams,
-    { emitPartial, logger, makeRange }: Pick<ClientEnv<R>, 'logger' | 'makeRange'> & ObserveOptions
+    { logger, makeRange, emitPartial }: Pick<ClientEnv<R>, 'logger' | 'makeRange'> & ObserveOptions
 ): Observable<Annotation<R>[]> {
-    return providerClients.pipe(
-        mergeMap(providerClients =>
-            providerClients && providerClients.length > 0
-                ? combineLatest(
-                      providerClients.map(({ providerClient: { annotations }, settings }) =>
-                          annotations
-                              ? defer(() => from(annotations(params, settings))).pipe(
-                                      emitPartial ? startWith(null) : tap(),
-                                      catchError(error => {
-                                          logger?.(`failed to get annotations: ${error}`)
-                                          console.error(error)
-                                          return of(null)
+    return observeProviderCall(
+        providerClients,
+        ({ providerClient, settings }) =>
+            providerClient.annotations
+                ? from(providerClient.annotations(params, settings)).pipe(
+                      map(anns =>
+                          anns
+                              ? anns
+                                      .map(ann => ({
+                                          ...ann,
+                                          range: ann.range ? makeRange(ann.range) : undefined,
+                                      }))
+                                      .sort((a, b) => {
+                                          const lineCmp =
+                                              (a.range?.start.line ?? 0) - (b.range?.start.line ?? 0)
+                                          if (lineCmp !== 0) {
+                                              return lineCmp
+                                          }
+                                          return (
+                                              (a.range?.start.character ?? 0) -
+                                              (b.range?.start.character ?? 0)
+                                          )
                                       })
-                                  )
-                              : of(null)
+                              : null
                       )
                   )
-                : of([])
-        ),
-        map(result => result.filter((v): v is AnnotationsResult => v !== null).flat()),
-        map(anns =>
-            anns
-                .map(ann => ({ ...ann, range: ann.range ? makeRange(ann.range) : undefined }))
-                .sort((a, b) => {
-                    const lineCmp = (a.range?.start.line ?? 0) - (b.range?.start.line ?? 0)
-                    if (lineCmp !== 0) {
-                        return lineCmp
-                    }
-                    return (a.range?.start.character ?? 0) - (b.range?.start.character ?? 0)
-                })
-        ),
-        tap(anns => {
-            if (LOG_VERBOSE) {
-                logger?.(`got ${anns.length} annotations: ${JSON.stringify(anns)}`)
-            }
-        })
+                : of(null),
+        { logger, emitPartial }
     )
 }
 
