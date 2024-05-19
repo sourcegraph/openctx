36bbee14c66d2a60331b31dd93960ced26f58435
Quinn Slack <quinn@slack.org>
2024-05-19 13:34:54 -0700

    improve provider build/publish steps
    - use unpkg instead of esm.sh
    - standardize bundling build steps
    - document how to build and publish a provider

--- web/pages/playground/data.ts
+++ web/pages/playground/data.ts
@@ -48,7 +48,7 @@ function providerBundleUrl(packageName: string, importUrl: string): string {
 
 const settings = {
     'openctx.providers': {
-        [providerBundleUrl('@openctx/provider-links@0.0.10', providerLinksUrl)]: {
+        [providerBundleUrl('@openctx/provider-links@0.0.12', providerLinksUrl)]: {
             links: [
                 {
                     title: 'Telemetry',
@@ -73,7 +73,7 @@ const settings = {
                 },
             ],
         } satisfies import('@openctx/provider-links').Settings,
-        [providerBundleUrl('@openctx/provider-prometheus@0.0.8', providerPrometheusUrl)]: {
+        [providerBundleUrl('@openctx/provider-prometheus@0.0.10', providerPrometheusUrl)]: {
             metricRegistrationPatterns: [
                 {
                     path: '**/*.ts?(x)',
@@ -83,7 +83,7 @@ const settings = {
                 },
             ],
         } satisfies import('@openctx/provider-prometheus').Settings,
-        [providerBundleUrl('@openctx/provider-storybook@0.0.8', providerStorybookUrl)]: {
+        [providerBundleUrl('@openctx/provider-storybook@0.0.10', providerStorybookUrl)]: {
             storybookUrl:
                 'https://daeeaa811098f52f15a110dbaf76b6c416191c3b--5f0f381c0e50750022dc6bf7.chromatic.com/', // this is a public URL because our storybooks are public
         } satisfies import('@openctx/provider-storybook').Settings,
