ff986e4213fb7b97b64767b94dc126fe762a6b00
Tim Lucas <t@toolmantim.com>
2024-05-27 17:24:02 +0800

    Stub

---
+++ provider/jira/README.md
@@ -0,0 +1,26 @@
+# JIRA context provider for OpenCtx
+
+This is a context provider for [OpenCtx](https://openctx.org) that brings JIRA context to code AI and editors. 
+
+**Status:** Experimental
+
+## Configuration
+
+[Create an API token](https://id.atlassian.com/manage-profile/security/api-tokens) and then use the following OpenCtx provider configuration:
+
+```json
+"openctx.providers": {
+    // ...other providers...
+    "https://openctx.org/npm/@openctx/provider-jira": {
+        "endpoint": "https://<subdomain>.atlassian.net/rest/api/",
+        "username": "<email-address>",
+        "apiToken": "<your-atlassian-api-token>",
+    }
+},
+```
+
+## Development
+
+- [Source code](https://sourcegraph.com/github.com/sourcegraph/openctx/-/tree/provider/jira)
+- [Docs](https://openctx.org/docs/providers/jira)
+- License: Apache 2.0
---
+++ provider/jira/index.ts
@@ -0,0 +1,60 @@
+import type {
+    Annotation,
+    AnnotationsParams,
+    AnnotationsResult,
+    ItemsParams,
+    ItemsResult,
+    MetaParams,
+    MetaResult,
+    Provider,
+    ProviderSettings,
+} from '@openctx/provider'
+
+const jiraProvider: Provider = {
+    meta(params: MetaParams, settings: ProviderSettings): MetaResult {
+        return { name: 'JIRA', features: { mentions: true } }
+    },
+
+    items(params: ItemsParams, settings: ProviderSettings): ItemsResult {
+        return [
+            {
+                title: '✨ Hello, world!',
+                url: 'https://openctx.org',
+                ui: {
+                    hover: { text: 'From OpenCtx' },
+                },
+                ai: {
+                    content: 'Hello, world!',
+                },
+            },
+        ]
+    },
+
+    annotations(params: AnnotationsParams, settings: ProviderSettings): AnnotationsResult {
+        const lines = params.content.split('\n')
+        const anns: Annotation[] = []
+        for (const [i, line] of lines.entries()) {
+            if (i % 10 !== 0) {
+                continue
+            }
+            anns.push({
+                uri: params.uri,
+                range: {
+                    start: { line: i, character: 0 },
+                    end: { line: i, character: line.length },
+                },
+                item: {
+                    title: '✨ Hello, world!',
+                    url: 'https://openctx.org',
+                    ui: {
+                        hover: { text: 'From OpenCtx' },
+                    },
+                },
+            })
+        }
+
+        return anns
+    },
+}
+
+export default jiraProvider
