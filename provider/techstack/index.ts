ecec6ab684e325711a970fb0bc988e5ceb4af21c
Harish Navnit <harishnavnit@gmail.com>
2024-05-27 21:46:23 +0800

    Refactor++

--- provider/techstack/index.ts
+++ provider/techstack/index.ts
@@ -1,40 +1,16 @@
 
-import fs from 'fs'
-import YAML from 'yaml'
+import load from './settings.js'
+
 import type {
+    Annotation,
     AnnotationsParams,
     AnnotationsResult,
     MetaParams,
     MetaResult,
-    Provider,
-    Annotation
+    Provider
 } from '@openctx/provider'
-
-import type TSF from './techstack.schema.ts'
 export type Settings = { yaml: string }
 
-/**
- * Read the techstack file configuration for project
- *
- * @param fileUri - absolute path of techstack yml file
- * @returns parsed yaml object
- */
-async function load(fileUri: string): Promise<TSF> {
-    let content
-    if (typeof window === 'undefined') {
-        // Server
-        content = await fs.promises.readFile(fileUri, 'utf-8')
-    } else {
-        // Browser
-        const r: Response = await fetch(fileUri)
-        if (r.status !== 200) {
-            console.error(`Techstack: failed to fetch settings from ${fileUri}`)
-            return {} as TSF
-        }
-        content = await r.text()
-    }
-    return YAML.parse(content)
-}
 
 const techstack: Provider<Settings> = {
     meta(params: MetaParams, settings: Settings): MetaResult {
