f0844c040ff4b7f2c1a4efe214291c895a565a48
Hitesh Sagtani <sagtanih@gmail.com>
2024-05-28 11:07:41 +0800

    Implement slack context provider (#84)
    - Implements Slack context provider for OpenCtx, which allows to search
    for relevant threads when mentioning a slack channel and use all the
    messages in the thread as the context to AI.
    - Uses [@slack/web-api](https://www.npmjs.com/package/@slack/web-api) to
    fetch all data related to slack such as channel list, use slack search
    api and get thread messages etc.
    - Requires authorization using a personal User API token created via
    slack app.
    
    ## Test Plan
    - Tested using a personal user auth token created via slack app.
    
    ## ToDo
    1. Add a local setup to handle the `https` redirects from slack to get
    the user auth token.
    2. For now, we use a common user access token, with the limitation that
    we can only access public channels and not private channels.
    
    ## Demo
    
    
    https://github.com/sourcegraph/openctx/assets/20701220/15b6096f-ff1c-42dd-ba66-961fa7cbeebe

---
+++ provider/slack-channel-context/index.ts
@@ -0,0 +1,195 @@
+import type {
+    ItemsParams,
+    ItemsResult,
+    MentionsParams,
+    MentionsResult,
+    MetaResult,
+} from '@openctx/provider'
+import { WebClient } from '@slack/web-api'
+import type { Match } from '@slack/web-api/dist/types/response/SearchFilesResponse.js'
+
+interface ChannelInfo {
+    name: string
+    id: string
+    url: string
+}
+
+export type Settings = {
+    slackAuthToken: string
+}
+
+function getSlackClient(settingsInput: Settings) {
+    if (settingsInput.slackAuthToken) {
+        const slackWebClient = new WebClient(settingsInput.slackAuthToken)
+        return slackWebClient
+    }
+    throw new Error('must provide a Slack user auth token in the `slackAuthToken` user settings field')
+}
+
+async function listAllChannels(client: WebClient): Promise<ChannelInfo[]> {
+    let allChannels: ChannelInfo[] = []
+    let cursor: string | undefined
+    do {
+        const response = await client.conversations.list({
+            limit: 1000,
+            cursor: cursor,
+        })
+
+        if (response.channels) {
+            const channelInfo = response.channels.map(channel => ({
+                name: channel.name || '',
+                id: channel.id || '',
+                url: `https://slack.com/archives/${channel.id}`,
+            }))
+            allChannels = [...allChannels, ...channelInfo]
+        }
+        cursor = response.response_metadata?.next_cursor
+    } while (cursor)
+    return allChannels
+}
+
+// Function to search messages in a channel
+async function searchInChannel(
+    client: WebClient,
+    channelId: string,
+    query: string
+): Promise<Match[] | undefined> {
+    try {
+        const result = await client.search.messages({ query: `in:<#${channelId}> ${query}`, count: 50 })
+        return result?.messages?.matches
+    } catch (error) {
+        return undefined
+    }
+}
+
+async function fetchThreadMessages(
+    client: WebClient,
+    channelId: string,
+    threadTs: string
+): Promise<string | null> {
+    const allMessages = []
+    try {
+        let response = await client.conversations.replies({ channel: channelId, ts: threadTs })
+        let messages = response.messages as any[]
+        allMessages.push(...messages)
+        while (response.has_more) {
+            const nextCursor = response.response_metadata?.next_cursor
+            response = await client.conversations.replies({
+                channel: channelId,
+                ts: threadTs,
+                cursor: nextCursor,
+            })
+            messages = response.messages as any[]
+            allMessages.push(...messages)
+        }
+        return allMessages?.map(msg => msg.text).join('\n\n')
+    } catch (error) {
+        return null
+    }
+}
+
+interface SlackChannelData {
+    isInitialized: boolean
+    channelList: ChannelInfo[]
+}
+
+let slackChannelData: SlackChannelData = {
+    isInitialized: false,
+    channelList: [],
+}
+
+const slackContext = {
+    meta(): MetaResult {
+        return { name: 'Slack Context', features: { mentions: true } }
+    },
+
+    async initializeChannelList(settingsInput: Settings) {
+        if (slackChannelData.isInitialized === false) {
+            const client = getSlackClient(settingsInput)
+            const channelList = await listAllChannels(client)
+            slackChannelData = {
+                isInitialized: true,
+                channelList: channelList,
+            }
+        }
+    },
+
+    async mentions(params: MentionsParams, settingsInput: Settings): Promise<MentionsResult> {
+        await this.initializeChannelList(settingsInput)
+        const userQuery = params.query ?? ''
+        const channelIdList = slackChannelData.channelList.filter(channel =>
+            channel.name.includes(userQuery)
+        )
+        if (!channelIdList) {
+            return []
+        }
+        const mentionRes: MentionsResult = []
+        for (const channel of channelIdList) {
+            mentionRes.push({
+                title: channel.name,
+                uri: channel.url,
+                data: {
+                    channelId: channel.id,
+                },
+            })
+        }
+        return mentionRes
+    },
+
+    async items(params: ItemsParams, settingsInput: Settings): Promise<ItemsResult> {
+        const client = getSlackClient(settingsInput)
+
+        const channelId = params.mention?.data?.channelId as string
+        if (channelId === undefined || channelId === null) {
+            return []
+        }
+        let message = params.message || ''
+        const mentionUrl = `@openctx:${params.mention?.uri}` || ''
+        if (message.startsWith(mentionUrl)) {
+            message = message.slice(mentionUrl.length)
+        }
+        // Slack api does not expose the `ts` property via the interface which is required to get the slack thread messages
+        const searchResults = (await searchInChannel(client, channelId, message)) as any[]
+
+        if (!searchResults) {
+            return []
+        }
+
+        const NUM_SEARCH_RESULTS = 10
+        if (searchResults.length > NUM_SEARCH_RESULTS) {
+            searchResults.splice(NUM_SEARCH_RESULTS)
+        }
+        const allPromises = searchResults.map(async searchResult => {
+            const threadTs = searchResult.ts as string
+            const link = searchResult.permalink as string
+
+            const threadMessage = await fetchThreadMessages(client, channelId, threadTs)
+            const allMessages = `
+                Here is a conversation from the slack message which could be useful to answer the question.
+                Please use the context from the slack conversation to check if it helps.
+                Please ignore the slack conversation if it is not relevant to the query.
+            
+
+                ${threadMessage}
+            `
+            if (allMessages) {
+                return {
+                    url: link,
+                    title: allMessages,
+                    ai: { content: allMessages },
+                }
+            }
+            return undefined
+        })
+        const allMessages = await Promise.all(allPromises)
+        const allSlackContext = []
+        for (const message of allMessages) {
+            if (message) {
+                allSlackContext.push(message)
+            }
+        }
+        return allSlackContext
+    },
+}
+
+export default slackContext
