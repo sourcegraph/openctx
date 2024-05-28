f76b1854a4f9f4ed50ddcc032a952a6635f553b7
Valery Bugakov <skymk1@gmail.com>
2024-05-28 13:55:57 +0800

    Provider: remove `@linear/sdk` from the Linear provider (#90)
    - Removes `@linear/sdk` from the Linear provider to fix bundling issues
    and reduce the bundle size
    - Published a new version with the changes in this PR
    - Added the provider config to Linear for dogfooding.

--- provider/linear/index.ts
+++ provider/linear/index.ts
@@ -7,18 +7,30 @@ import type {
     MetaResult,
     Provider,
 } from '@openctx/provider'
-
-import { LinearClient, type LinearClientOptions } from '@linear/sdk'
 import { XMLBuilder } from 'fast-xml-parser'
+
 import type { UserCredentials } from './auth.js'
 
 /** Settings for the Linear OpenCtx provider. */
 export type Settings = {
     linearUserCredentialsPath?: string
-    linearClientOptions?: LinearClientOptions
+    linearClientCredentials?: { accessToken?: string }
 }
 
 const xmlBuilder = new XMLBuilder({ format: true })
+
+interface Issue {
+    title: string
+    url: string
+    description: string
+    comments?: {
+        nodes: Comment[]
+    }
+}
+
+interface Comment {
+    body: string
+}
 
 const linearIssues: Provider<Settings> = {
     meta(): MetaResult {
@@ -26,14 +38,25 @@ const linearIssues: Provider<Settings> = {
     },
 
     async mentions(params: MentionsParams, settingsInput: Settings): Promise<MentionsResult> {
-        if (!params.query) {
+        if (!params.query || params.query.length < 3) {
             return []
         }
 
-        const linearClient = getLinearClient(settingsInput)
-        const issues = await linearClient.issueSearch({ query: params.query, first: 25 })
+        const query = `
+            query IssueSearch($query: String!, $first: Int!) {
+                issueSearch(query: $query, first: $first, orderBy: updatedAt) {
+                    nodes {
+                        title
+                        url
+                    }
+                }
+            }
+        `
+        const variables = { query: params.query, first: 10 }
+        const response = await linearApiRequest(query, variables, settingsInput)
+        const issues = response.data.issueSearch.nodes as Issue[]
 
-        return (issues.nodes ?? []).map(issue => ({
+        return (issues ?? []).map(issue => ({
             title: issue.title,
             uri: issue.url,
         }))
@@ -49,14 +72,29 @@ const linearIssues: Provider<Settings> = {
             return []
         }
 
-        const linearClient = getLinearClient(settingsInput)
-
-        const issue = await linearClient.issue(issueId)
-        const comments = await issue.comments()
+        const query = `
+            query Issue($id: String!) {
+                issue(id: $id) {
+                    title
+                    description
+                    comments {
+                        nodes {
+                            body
+                        }
+                    }
+                }
+            }
+        `
+        const variables = { id: issueId }
+        const data = await linearApiRequest(query, variables, settingsInput)
+        const issue = data.data.issue as Issue
+        const comments = issue.comments?.nodes as Comment[]
 
         const content = xmlBuilder.build({
-            description: issue.description || '',
-            comments: comments.nodes.map(comment => comment.body).join('\n'),
+            linear_issue: {
+                description: issue.description || '',
+                comments: comments.map(comment => comment.body).join('\n'),
+            },
         })
 
         return [
@@ -73,7 +111,11 @@ const linearIssues: Provider<Settings> = {
 
 export default linearIssues
 
-function getLinearClient(settings: Settings): LinearClient {
+function getAccessToken(settings: Settings): string {
+    if (settings.linearClientCredentials?.accessToken) {
+        return settings.linearClientCredentials.accessToken
+    }
+
     if (settings.linearUserCredentialsPath) {
         const userCredentialsString = readFileSync(settings.linearUserCredentialsPath, 'utf-8')
         const userCredentials = JSON.parse(userCredentialsString) as Partial<UserCredentials>
@@ -82,20 +124,40 @@ function getLinearClient(settings: Settings): LinearClient {
             throw new Error(`access_token not found in ${settings.linearUserCredentialsPath}`)
         }
 
-        return new LinearClient({ accessToken: userCredentials.access_token })
-    }
-
-    if (settings.linearClientOptions?.accessToken) {
-        return new LinearClient({ accessToken: settings.linearClientOptions?.accessToken })
-    }
-
-    if (settings.linearClientOptions?.apiKey) {
-        return new LinearClient({ apiKey: settings.linearClientOptions?.apiKey })
+        return userCredentials.access_token
     }
 
     throw new Error(
-        'must provide a Linear user credentials path in the `linearUserCredentialsPath` settings field or a path to a JSON file in the LINEAR_USER_CREDENTIALS_FILE env var'
+        'must provide a Linear user credentials path in the `linearUserCredentialsPath` settings field or an accessToken in the linearClientOptions'
     )
+}
+
+async function linearApiRequest(
+    query: string,
+    variables: object,
+    settings: Settings
+): Promise<{ data: any }> {
+    const accessToken = getAccessToken(settings)
+    const response = await fetch('https://api.linear.app/graphql', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({ query, variables }),
+    })
+
+    if (!response.ok) {
+        throw new Error(`Linear API request failed: ${response.statusText}`)
+    }
+
+    const json = (await response.json()) as { data: object }
+
+    if (!json.data) {
+        throw new Error('Linear API request failed: no data')
+    }
+
+    return json
 }
 
 function parseIssueIDFromURL(urlStr: string): string | undefined {
