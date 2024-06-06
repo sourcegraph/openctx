8116ecf46427da90c04ca3de9e2488fd0dd70a84
dyma solovei <dmytro.y.solovei@gmail.com>
2024-06-06 14:00:29 +0200

    feat(pprof): get `pprof -top` for current package
    This makes sure the annotations are only set for the most "resource
    hungry" functions and not for each function in the package.

--- provider/pprof/pprof.ts
+++ provider/pprof/pprof.ts
@@ -3,9 +3,30 @@ import { readdirSync } from 'fs'
 import { dirname, join } from 'path'
 import { matchGlob } from '@openctx/provider'
 
+/**
+ * topNodeRegex is the output in `pprof -top` command for a single node
+ *
+ * An example output would look like this (first row for reference only):
+ *
+ * ```
+ *  flat  flat%   sum%        cum   cum%
+ *  5.59s 42.93% 43.16%      6.21s 47.70%  container/list.(*List).Init
+ * ```
+ *
+ * @see https://regex101.com/r/26v9rd/1
+ */
+const topNodeRegex =
+    /^ +(?<flat>[\d\\.]+)(?<unit>\w) +(?<flatPerc>[\d\\.]+)% +(?:[\d\\.]+)% +(?<cum>[\d\\.]+)(?:\w) +(?<cumPerc>[\d\\.]+)% +(?<func>[\w\\.\\(\\*\\)\\/]+)/
+
+/**
+ * PprofTool is a CLI tool for reading and visualizing profile reports.
+ * `pprof` is bundled with every `go` binary, but may also be installed as a standalone tool.
+ */
+export type PprofTool = 'go tool pprof' | 'pprof'
+
 export function getPprof(): Pprof | null {
     let hasGo = false
-    exec('which go', (error, stdout, stderr) => {
+    exec('which go', (error, stdout, _stderr) => {
         if (error !== null || stdout.endsWith('not found')) {
             return
         }
@@ -15,7 +36,7 @@ export function getPprof(): Pprof | null {
     if (!hasGo) {
         return null
     }
-    return new Pprof()
+    return new Pprof('go tool pprof')
 }
 
 interface SearchOptions {
@@ -71,4 +92,137 @@ export function findReportPath(currentDir: string, options: SearchOptions): stri
     return report
 }
 
-export class Pprof {}
+export interface TopOptions {
+    /** package limits the entries to the current package (`-show="^package\."`)*/
+    package: string
+
+    /** nodeCount limits the number of results returned (`-nodecount=x`). Skipped if not set. */
+    nodeCount?: number
+
+    /** excludeInline excludes inline function calls from the output (`-noinlines`) */
+    excludeInline?: boolean
+
+    /** sort controls how nodes are sorted in the output (`-flat` or `-cum`) */
+    sort?: 'flat' | 'cum'
+}
+
+export interface TopOutput {
+    /** Name of the binary used to generate the report. If no binary was passed to `pprof` command, this field is `undefined`. */
+    file?: string
+    type: string
+    unit: string
+    nodes: Node[]
+}
+
+export interface Node {
+    function: string
+
+    /** Absolute CPU time/ memory associated with this function only. */
+    flat: number
+
+    /** Relative CPU time / memory associated with this function only. */
+    flatPerc: number
+
+    /** CPU time / memory associated with the function and its descendants. */
+    cum: number
+
+    /** Relative CPU time / memory associated with the function and its descendants. */
+    cumPerc: number
+}
+
+/**
+ * Pprof is a wrapper for working with `pprof` CLI.
+ */
+export class Pprof {
+    private tool: PprofTool
+    private report: string | null = null
+    private binary?: string
+
+    constructor(tool: PprofTool) {
+        this.tool = tool
+    }
+
+    setReport(path: string) {
+        this.report = path
+    }
+
+    setBinary(path?: string) {
+        this.binary = path
+    }
+
+    top(options: TopOptions): TopOutput | null {
+        if (!this.report) {
+            return null
+        }
+
+        const cmd = this.topCmd(options)
+        let out: string | null = null
+        exec(cmd, (error, stdout, stderr) => {
+            if (error !== null || stderr.endsWith('no such file or directory')) {
+                return
+            }
+            out = stdout
+        })
+
+        if (out === null) {
+            return null
+        }
+
+        return this.parseTop(out)
+    }
+
+    private topCmd(options: TopOptions): string {
+        let cmd = this.tool + ` -top -show="${options.package}\\."`
+        cmd += options.sort ? ` -${options.sort}` : ' -cum'
+
+        if (options.excludeInline === undefined || options.excludeInline === true) {
+            cmd += ' -noinlines'
+        }
+
+        if (this.binary) {
+            cmd += ` ${this.binary}`
+        }
+        cmd += ` ${this.report}`
+        return cmd
+    }
+
+    private parseTop(output: string): TopOutput | null {
+        const binaryName = /^File: ([\w-\\/]+)/m.exec(output)
+        const reportType = /^Type: (\w+)/m.exec(output)
+
+        let unit: string | null = null
+        const nodes: Node[] = []
+
+        const startPos = output.search(/ +flat +flat% +sum% +cum +cum%/)
+        if (startPos === -1) {
+            return null
+        }
+        const lines = output.substring(startPos).split('\n').slice(1)
+        for (const line of lines) {
+            const match = topNodeRegex.exec(line)
+            if (match === null || !match.groups) {
+                continue
+            }
+            const { groups: node } = match
+
+            if (unit === null) {
+                unit = node.unit
+            }
+
+            nodes.push({
+                function: node.func,
+                flat: parseFloat(node.flat),
+                flatPerc: parseFloat(node.flatPerc),
+                cum: parseFloat(node.cum),
+                cumPerc: parseFloat(node.cumPerc),
+            })
+        }
+
+        return {
+            type: reportType ? reportType[1] || 'cpu' : 'cpu',
+            file: binaryName ? binaryName[1] : undefined,
+            unit: unit || 's',
+            nodes: nodes,
+        }
+    }
+}
