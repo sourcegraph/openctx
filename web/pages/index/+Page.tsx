2da161d8b7e539345c89b68ba9dfe1f7bd6e6dd9
Tim Lucas <t@toolmantim.com>
2024-06-13 11:30:54 +1000

    web: remove links to pages that don't exist (#146)
    Fixes
    https://linear.app/sourcegraph/issue/CODY-2099/context-providers-link-on-openctxorg-homepage-is-broken

--- web/pages/index/+Page.tsx
+++ web/pages/index/+Page.tsx
@@ -94,12 +94,7 @@ const IntegrationsList: FunctionComponent<{
 }> = ({ title, type, items, className }) => (
     <div className={clsx('flex flex-wrap items-start gap-4 sm:flex-nowrap', className)}>
         <h2 className="mt-[7px] shrink-0 whitespace-nowrap text-right text-xs sm:w-[120px] lg:text-sm">
-            <Link
-                href={`/${type}`}
-                className="font-bold text-foreground no-underline underline-offset-4 hover:underline"
-            >
-                {title}
-            </Link>
+            {title}
         </h2>
         <ul className="flex flex-wrap items-center gap-2">
             {items.map(item => (
